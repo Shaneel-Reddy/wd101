a0649921a2246271de8dc2ea6f4d0adc724c6802
L Shaneel Reddy <143787702+Shaneel-Reddy@users.noreply.github.com>
2024-09-14 12:38:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const displayEntries = () => {
         const emailCell = `<td class="border px-4 py-2">${entry.email}</td>`;
         const passwordCell = `<td class="border px-4 py-2">${entry.password}</td>`;
         const dobCell = `<td class="border px-4 py-2">${entry.dob}</td>`;
-        const acceptTermsCell = `<td class="border px-4 py-2">${entry.acceptedTerms ? "Yes" : "No"}</td>`;
+        const acceptTermsCell = `<td class="border px-4 py-2">${entry.acceptedTerms ? "true" : "false"}</td>`;
 
         const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
         return row;
